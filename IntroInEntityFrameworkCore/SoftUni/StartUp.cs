63f31f0b775b4afaa39086ad121881638f222c09
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-02-18 22:11:12 +0200

    The first two tasks have been uploaded

---
+++ IntroInEntityFrameworkCore/SoftUni/StartUp.cs
@@ -0,0 +1,62 @@
+﻿using SoftUni.Data;
+using SoftUni.Models;
+using System;
+using System.Linq;
+using System.Text;
+
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            string result = GetEmployeesWithSalaryOver50000(context);
+
+            Console.WriteLine(result);
+        }
+
+        //Task 3
+        public static string GetEmployeesFullInformation(SoftUniContext context) 
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees.Select(e => new
+            {
+                FirstName = e.FirstName,
+                MiddleName = e.MiddleName,
+                LastName = e.LastName,
+                JobTitle = e.JobTitle,
+                Salary = e.Salary
+            }).ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        // Task 4
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context) 
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees.Where(e => e.Salary > 50000).Select(e => new
+            {
+                e.FirstName,
+                e.Salary
+            }).OrderBy(e => e.FirstName).ToList();
+
+            foreach (var employee in employees)
+            {
+                sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
