fe5a8d82cd83b1f5f4a0b28b3d4715041e777c0f
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-02-20 10:14:48 +0200

    New method has been added

--- IntroInEntityFrameworkCore/SoftUni/StartUp.cs
+++ IntroInEntityFrameworkCore/SoftUni/StartUp.cs
@@ -13,7 +13,7 @@ namespace SoftUni
         {
             SoftUniContext context = new SoftUniContext();
 
-            string result = GetEmployeesWithSalaryOver50000(context);
+            string result = GetEmployeesFromResearchAndDevelopment(context);
 
             Console.WriteLine(result);
         }
@@ -54,6 +54,31 @@ namespace SoftUni
             foreach (var employee in employees)
             {
                 sb.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        // Task 5
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = context.Employees.Where(e => e.Department.Name == "Research and Development")
+                                                        .Select(e => new
+                                                        {
+                                                            e.FirstName,
+                                                            e.LastName,
+                                                            DepartmentName = e.Department.Name,
+                                                            e.Salary
+                                                        }).OrderBy(e => e.Salary)
+                                                            .ThenByDescending(e => e.FirstName)
+                                                            .ToList();
+
+            foreach (var e in employees)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.DepartmentName} - ${e.Salary:f2}");
             }
 
             return sb.ToString().TrimEnd();
