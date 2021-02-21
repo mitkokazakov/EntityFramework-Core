cea23cf05fe8840d27b86bfcc66b04fa629e27c0
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-02-21 10:45:29 +0200

    Task 8 has been added

--- IntroInEntityFrameworkCore/SoftUni/StartUp.cs
+++ IntroInEntityFrameworkCore/SoftUni/StartUp.cs
@@ -156,5 +156,30 @@ namespace SoftUni
 
             return sb.ToString().TrimEnd();
         }
+
+        // Task 8
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var addresses = context.Addresses.Select(a => new
+            {
+                a.AddressText,
+                TownName = a.Town.Name,
+                EmployeesCount = a.Employees.Count
+            }).OrderByDescending(a => a.EmployeesCount)
+                                      .ThenBy(a => a.TownName)
+                                      .ThenBy(a => a.AddressText)
+                                      .Take(10)
+                                      .ToList();
+
+            foreach (var a in addresses)
+            {
+                sb.AppendLine($"{a.AddressText}, {a.TownName} - {a.EmployeesCount} employees");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
