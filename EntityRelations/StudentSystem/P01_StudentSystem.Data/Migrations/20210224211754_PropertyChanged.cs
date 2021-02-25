27e6e5747d8b55db752c45651b0f1e57983c1956
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-02-25 20:48:18 +0200

    All migrations have been added

---
+++ EntityRelations/StudentSystem/P01_StudentSystem.Data/Migrations/20210224211754_PropertyChanged.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P01_StudentSystem.Data.Migrations
+{
+    public partial class PropertyChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SubmissionTime",
+                table: "Homeworks",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "SubmissionTime",
+                table: "Homeworks",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+        }
+    }
+}
---
+++ EntityRelations/StudentSystem/P01_StudentSystem.Data/Migrations/20210224213857_WrongPropName.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P01_StudentSystem.Data.Migrations
+{
+    public partial class WrongPropName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Resources",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "ResoucreId",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "ResoucreType",
+                table: "Resources");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResourceId",
+                table: "Resources",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResourceType",
+                table: "Resources",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Resources",
+                table: "Resources",
+                column: "ResourceId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Resources",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "ResourceId",
+                table: "Resources");
+
+            migrationBuilder.DropColumn(
+                name: "ResourceType",
+                table: "Resources");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResoucreId",
+                table: "Resources",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ResoucreType",
+                table: "Resources",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Resources",
+                table: "Resources",
+                column: "ResoucreId");
+        }
+    }
+}
