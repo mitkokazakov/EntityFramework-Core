dd0cf8e6902610df305ffbfe9019a83f0197a3fa
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-03-01 22:57:24 +0200

    Add all model classes

---
+++ Linq/P01_HospitalDatabase/Data/Models/Diagnose.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Diagnose
+    {
+        [Key]
+        public int DiagnoseId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Comments { get; set; }
+
+        [Required]
+        [ForeignKey("Patient")]
+        public int PatientId { get; set; }
+
+        [Required]
+        public virtual Patient Patient { get; set; }
+    }
+}
---
+++ Linq/P01_HospitalDatabase/Data/Models/PatientMedicament.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class PatientMedicament
+    {
+        [Required]
+        [ForeignKey("Patient")]
+        public int PatientId { get; set; }
+
+        [Required]
+        public virtual Patient Patient { get; set; }
+
+        [Required]
+        [ForeignKey("Medicament")]
+        public int MedicamentId { get; set; }
+
+        [Required]
+        public virtual Medicament Medicament { get; set; }
+    }
+}
