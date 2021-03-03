0496d58ca84a390b2ab85b0761cb17074215911d
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-03-03 23:27:57 +0200

    All model classes have been uploaded

---
+++ Linq/P03_SalesDatabase/Data/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            this.Sales = new HashSet<Sale>();
+        }
+
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Quantity { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [MaxLength(250)]
+        public string Description { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
---
+++ Linq/P03_SalesDatabase/Data/Models/Store.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P03_SalesDatabase.Data.Models
+{
+    public class Store
+    {
+        public Store()
+        {
+            this.Sales = new HashSet<Sale>();
+        }
+
+        [Key]
+        public int StoreId { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
