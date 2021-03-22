fff5f2d571907c1f45ed1b29f876a1be0524e4b7
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-03-22 22:52:40 +0200

    Data project created and RealEstateDbContext class

---
+++ RealEstate/RealEstates.Data/RealEstateDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using RealEstates.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstates.Data
+{
+    public class RealEstateDbContext : DbContext
+    {
+        public RealEstateDbContext()
+        {
+
+        }
+        public RealEstateDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Estate> Properties { get; set; }
+        public DbSet<District> Districts { get; set; }
+        public DbSet<TypeBuilding> TypesBuilding { get; set; }
+        public DbSet<TypeProperty> TypesProperty { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RealEstate;Integrated Security=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PropertiesTags>(entity =>
+            {
+                entity.HasKey(x => new { x.TagId, x.PropertyId });
+            });
+        }
+    }
+}
