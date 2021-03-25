985bad2c5e0b669ef723fab645ce1353a0e874bf
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-03-25 21:54:23 +0200

    Add Services project file, AutoMapperConfig class and RealEstatesProfiler class

---
+++ RealEstate/RealEstates.Services/AutoMapperConfig.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstates.Services
+{
+    public abstract class AutoMapperConfig
+    {
+        public AutoMapperConfig()
+        {
+            this.Initializer();
+        }
+
+        protected IMapper Mapper { get; private set; }
+
+        private void Initializer()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<RealEstatesProfiler>();
+            });
+
+            this.Mapper = config.CreateMapper();
+        }
+    }
+}
---
+++ RealEstate/RealEstates.Services/RealEstatesProfiler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using RealEstates.Models;
+using RealEstates.Services.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RealEstates.Services
+{
+    public class RealEstatesProfiler : Profile
+    {
+        public RealEstatesProfiler()
+        {
+            this.CreateMap<Estate, PropertyViewModel>()
+                .ForMember(x => x.District, y => y.MapFrom(x => x.District.Name))
+                .ForMember(x => x.BuildingType, y => y.MapFrom(x => x.TypeBuilding.Name))
+                .ForMember(x => x.PropertyType, y => y.MapFrom(x => x.TypeProperty.Name))
+                .ForMember(x => x.Floor, y => y.MapFrom(x => (x.Floor ?? 0).ToString() + "/" + (x.TotalFloors ?? 0).ToString()));
+
+            this.CreateMap<District, DistrictViewModel>()
+                .ForMember(x => x.MinValue, y => y.MapFrom(x => x.Properties.Min(p => p.Price)))
+                .ForMember(x => x.MaxValue, y => y.MapFrom(x => x.Properties.Max(p => p.Price)))
+                .ForMember(x => x.TotalEstates, y => y.MapFrom(x => x.Properties.Count()));
+        }
+    }
+}
