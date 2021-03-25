7d583e6da8eeef48ccd0f20d32f19d91d08196b5
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-03-25 21:58:48 +0200

    Interfaces for District and Property classes added

---
+++ RealEstate/RealEstates.Services/IDistrictsService.cs
@@ -0,0 +1,13 @@
+﻿using RealEstates.Services.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstates.Services
+{
+    public interface IDistrictsService
+    {
+        ICollection<DistrictViewModel> InfoForDistrict();
+        ICollection<DistrictViewModel> DistrictWithNewestEstates();
+    }
+}
---
+++ RealEstate/RealEstates.Services/IPropertiesService.cs
@@ -0,0 +1,18 @@
+﻿using RealEstates.Services.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstates.Services
+{
+    public interface IPropertiesService
+    {
+        void Create(string district,int size,int? floor,int? maxFloors,int? year, string propertyType,string buildingType,int price);
+
+        IEnumerable<PropertyViewModel> SearchByPrice(int minPrice, int maxPrice);
+
+        IEnumerable<PropertyViewModel> SearchBySizeAndYear(int minYear, int maxYear, int minSize, int mazSize);
+
+        void UpdateTags(int id);
+    }
+}
