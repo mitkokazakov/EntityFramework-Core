4f45a7e4cc6352c4893e14158a6f1a4f564f8009
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-03-27 14:09:26 +0200

    View Models added

---
+++ RealEstate/RealEstates.Services/ViewModels/DistrictViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealEstates.Services.ViewModels
+{
+    public class DistrictViewModel
+    {
+        public string Name { get; set; }
+
+        public int MinValue { get; set; }
+
+        public int MaxValue { get; set; }
+
+        public int TotalEstates { get; set; }
+    }
+}
