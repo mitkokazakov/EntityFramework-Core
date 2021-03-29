a513c284fe459c3e90ac2245dad1ff6620386e30
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-03-29 18:48:38 +0300

    HomeController added

---
+++ RealEstate/RealEstates.Web/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RealEstates.Services;
+using RealEstates.Web.Models;
+
+namespace RealEstates.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDistrictsService districtsService;
+        public HomeController(IDistrictsService districtsService)
+        {
+            this.districtsService = districtsService;
+        }
+
+        public IActionResult Index()
+        {
+            var districts = this.districtsService.DistrictWithNewestEstates();
+            return View(districts);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
