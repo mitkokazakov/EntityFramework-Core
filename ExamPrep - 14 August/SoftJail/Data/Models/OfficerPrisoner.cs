86cbe7febb8acb4f05ddad5e0c390203e11e6f8e
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-03-30 21:21:17 +0300

    All model classes added

---
+++ ExamPrep - 14 August/SoftJail/Data/Models/OfficerPrisoner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SoftJail.Data.Models
+{
+    public class OfficerPrisoner
+    {
+        [Required]
+        [ForeignKey("Officer")]
+        public int OfficerId { get; set; }
+
+        [Required]
+        public virtual Officer Officer { get; set; }
+
+        [Required]
+        [ForeignKey("Prisoner")]
+        public int PrisonerId { get; set; }
+
+        [Required]
+        public virtual Prisoner Prisoner { get; set; }
+    }
+}
