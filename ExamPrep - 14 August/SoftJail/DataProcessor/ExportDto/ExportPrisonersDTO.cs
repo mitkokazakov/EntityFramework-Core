f9b438f9772a94633a8bac0bd98f63c30fd3bd77
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-03-31 18:01:36 +0300

    Export DTO's classes added

---
+++ ExamPrep - 14 August/SoftJail/DataProcessor/ExportDto/ExportPrisonersDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftJail.DataProcessor.ExportDto
+{
+    public class ExportPrisonersDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CellNumber { get; set; }
+
+        public ExportOfficersDTO[] Officers { get; set; }
+
+        public decimal TotalOfficerSalary { get; set; }
+    }
+}
