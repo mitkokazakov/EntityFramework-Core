8280664e3d20ee100188201472ce5ebb5f282acb
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-03-31 18:01:05 +0300

    Import DTO's classes added

---
+++ ExamPrep - 14 August/SoftJail/DataProcessor/ImportDto/ImportOfficerPrisonerDTO.cs
@@ -0,0 +1,37 @@
+﻿using SoftJail.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Officer")]
+    public class ImportOfficerPrisonerDTO
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [Range(typeof(decimal), "0.00", "79228162514264337593543950335")]
+        [XmlElement("Money")]
+        public decimal Money { get; set; }
+
+        [Required]
+        [XmlElement("Position")]
+        public string Position { get; set; }
+
+        [Required]
+        [XmlElement("Weapon")]
+        public string Weapon { get; set; }
+
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public PrisonerDTO[] Prisoners { get; set; }
+    }
+}
---
+++ ExamPrep - 14 August/SoftJail/DataProcessor/ImportDto/PrisonerDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Prisoner")]
+    public class PrisonerDTO
+    {
+        [XmlAttribute("Id")]
+        public int Id { get; set; }
+    }
+}
