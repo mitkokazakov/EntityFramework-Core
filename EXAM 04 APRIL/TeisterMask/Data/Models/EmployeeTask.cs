8ada89cf67f6c3a704b0bfef4816be3a6a2c2730
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-04-06 21:53:56 +0300

    Add all model classes

---
+++ EXAM 04 APRIL/TeisterMask/Data/Models/EmployeeTask.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models
+{
+    public class EmployeeTask
+    {
+        [Required]
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; }
+
+        [Required]
+        public int TaskId { get; set; }
+        public virtual Task Task { get; set; }
+    }
+}
+
+//•	EmployeeId - integer, Primary Key, foreign key(required)
+//•	Employee -  Employee
+//•	TaskId - integer, Primary Key, foreign key(required)
+//•	Task - Task
