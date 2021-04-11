54468aacd40ba0b3b3acae69841fb6b7e4dd5a76
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-04-11 20:23:57 +0300

    Serializer and Deserializer classes added

---
+++ EXAM 04 APRIL/TeisterMask/DataProcessor/Deserializer.cs
@@ -0,0 +1,169 @@
+﻿namespace TeisterMask.DataProcessor
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+    using Data;
+    using Newtonsoft.Json;
+    using TeisterMask.Data.Models;
+    using TeisterMask.Data.Models.Enums;
+    using TeisterMask.DataProcessor.ImportDto;
+    using ValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedProject
+            = "Successfully imported project - {0} with {1} tasks.";
+
+        private const string SuccessfullyImportedEmployee
+            = "Successfully imported employee - {0} with {1} tasks.";
+
+        public static string ImportProjects(TeisterMaskContext context, string xmlString)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ImportProjectDTO[]), new XmlRootAttribute("Projects"));
+
+            ImportProjectDTO[] projects = (ImportProjectDTO[])serializer.Deserialize(new StringReader(xmlString));
+
+            List<Project> projToAdd = new List<Project>();
+
+            foreach (var pr in projects)
+            {
+                DateTime openDate;
+                bool isValidOpenDate = DateTime.TryParseExact(pr.OpenDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out openDate);
+
+                DateTime dueDate;
+                bool isValidDueDate = DateTime.TryParseExact(pr.DueDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dueDate);
+
+
+                if (!IsValid(pr) || !isValidOpenDate)
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Project project = new Project()
+                {
+                    Name = pr.Name,
+                    OpenDate = openDate,
+                    DueDate = dueDate,
+
+                };
+
+                foreach (var ts in pr.Tasks)
+                {
+
+                    DateTime openDateTask;
+                    bool isValidOpenDateTask = DateTime.TryParseExact(ts.OpenDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out openDateTask);
+
+                    DateTime dueDateTask;
+                    bool isValidDueDateTask = DateTime.TryParseExact(ts.DueDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dueDateTask);
+
+                    object execType;
+                    bool isValidExecType = Enum.TryParse(typeof(ExecutionType), ts.ExecutionType, out execType);
+
+                    object labelType;
+                    bool isValidLabelType = Enum.TryParse(typeof(LabelType), ts.LabelType, out labelType);
+
+                    if (!IsValid(ts) || !isValidDueDateTask || !isValidOpenDateTask || !isValidExecType || !isValidLabelType)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    if ((openDateTask < openDate) || (dueDateTask > dueDate))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Task task = new Task()
+                    {
+                        Name = ts.Name,
+                        OpenDate = openDateTask,
+                        DueDate = dueDateTask,
+                        ExecutionType = (ExecutionType)execType,
+                        LabelType = (LabelType)labelType
+                    };
+
+                    project.Tasks.Add(task);
+                }
+
+                projToAdd.Add(project);
+                sb.AppendLine(String.Format(SuccessfullyImportedProject, project.Name, project.Tasks.Count));
+            }
+
+            context.Projects.AddRange(projToAdd);
+            context.SaveChanges();
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportEmployees(TeisterMaskContext context, string jsonString)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var employees = JsonConvert.DeserializeObject<ImportEmployeeDTO[]>(jsonString);
+
+            foreach (var emp in employees)
+            {
+                if (!IsValid(emp))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Employee employee = new Employee()
+                {
+                    Username = emp.Username,
+                    Email = emp.Email,
+                    Phone = emp.Phone
+                };
+
+                var tasks = emp.Tasks.Distinct();
+
+                foreach (var ts in tasks)
+                {
+                    Task task = context.Tasks.FirstOrDefault(t => t.Id == ts);
+
+                    if (task == null)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    EmployeeTask employeeTask = new EmployeeTask()
+                    {
+                        Employee = employee,
+                        Task = task
+                    };
+
+                    employee.EmployeesTasks.Add(employeeTask);
+                }
+
+                context.Employees.Add(employee);
+                context.SaveChanges();
+                sb.AppendLine(String.Format(SuccessfullyImportedEmployee,employee.Username,employee.EmployeesTasks.Count));
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
---
+++ EXAM 04 APRIL/TeisterMask/DataProcessor/Serializer.cs
@@ -0,0 +1,76 @@
+﻿namespace TeisterMask.DataProcessor
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+    using Data;
+    using Newtonsoft.Json;
+    using TeisterMask.DataProcessor.ExportDto;
+    using Formatting = Newtonsoft.Json.Formatting;
+
+    public class Serializer
+    {
+        public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var projects = context.Projects.ToArray()
+                .Where(p => p.Tasks.Count >= 1)
+                .Select(p => new ExportProjectsDTO()
+                {
+                    TasksCount = p.Tasks.Count,
+                    ProjectName = p.Name,
+                    HasEndDate = p.DueDate.HasValue ? "Yes" : "No",
+                    Tasks = p.Tasks.Select(t => new ExportSingleTaskDTO()
+                    {
+                        Name = t.Name,
+                        Label = t.LabelType.ToString()
+                    }).OrderBy(t => t.Name).ToArray()
+                }).OrderByDescending(p => p.TasksCount)
+                .ThenBy(p => p.ProjectName)
+                .ToArray();
+
+
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ExportProjectsDTO[]), new XmlRootAttribute("Projects"));
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(new StringWriter(sb), projects, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
+        {
+
+            var employees = context.Employees
+                .Where(e => e.EmployeesTasks.Any(et => et.Task.OpenDate >= date))
+                .ToArray()
+                .Select(e => new ExportEmployeeDTO()
+                {
+                    Username = e.Username,
+                    Tasks = e.EmployeesTasks.Select(et => new ExportTaskDTO()
+                    {
+                        TaskName = et.Task.Name,
+                        OpenDate = et.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture),
+                        DueDate = et.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                        LabelType = et.Task.LabelType.ToString(),
+                        ExecutionType = et.Task.ExecutionType.ToString()
+                    }).OrderByDescending(t => t.DueDate)
+                    .ThenBy(t => t.TaskName)
+                    .ToArray()
+                }).OrderByDescending(e => e.Tasks.Count())
+                .ThenBy(e => e.Username)
+                .Take(10);
+
+            
+            string res = JsonConvert.SerializeObject(employees);
+
+            return res;
+        }
+    }
+}
