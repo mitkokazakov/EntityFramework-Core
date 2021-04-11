8ca0e7af22e9497651008e95e324c626f4abf1f1
mitkokazakov <DimitarKazakov@students.softuni.bg>
2021-04-11 20:24:45 +0300

    ExportDTO classes added

---
+++ EXAM 04 APRIL/TeisterMask/DataProcessor/ExportDto/ExportEmployeeDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeisterMask.DataProcessor.ExportDto
+{
+    public class ExportEmployeeDTO
+    {
+        public string Username { get; set; }
+
+        public ExportTaskDTO[] Tasks { get; set; }
+    }
+}
---
+++ EXAM 04 APRIL/TeisterMask/DataProcessor/ExportDto/ExportTaskDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TeisterMask.DataProcessor.ExportDto
+{
+    public class ExportTaskDTO
+    {
+        public string TaskName { get; set; }
+        public string OpenDate { get; set; }
+        public string DueDate { get; set; }
+        public string LabelType { get; set; }
+        public string ExecutionType { get; set; }
+    }
+}
